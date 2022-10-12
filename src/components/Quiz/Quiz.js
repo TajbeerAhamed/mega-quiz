f203be4939e5d68a8a0d8978afe556ee684a99dd
TajbeerAhamed <mdtajbeerahamedrimon@gmail.com>
2022-10-12 18:26:17 +0600

    heroicon-added

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import Option from './Option';
 import parse from 'html-react-parser';
+import { EyeIcon } from '@heroicons/react/24/solid'
 
 const Quiz = ({ topic }) => {
 
@@ -11,7 +12,8 @@ const Quiz = ({ topic }) => {
       <div className='flex justify-between mx-36'>
         <h1 className='font-poppins text-xl mb-4 font-semibold'>{parse(topic.question)}</h1>
         <p className='cursor-pointer' onClick={() => setRevealAns(true)}>
-          Eye
+        <EyeIcon className="h-6 w-6 mt-5 text-green-500"/>
+        
         </p>
       </div>
       <form className='grid grid-cols-2 gap-y-2 mb-4'>
